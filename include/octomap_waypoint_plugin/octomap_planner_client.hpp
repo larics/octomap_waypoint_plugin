8de0b5520d938909418e3ce2664a1721fa48dc32
Lovro Markovic <lovro.markovic@fer.hr>
2021-11-11 17:53:11 +0100

    Prepare for the planner service call

--- include/octomap_waypoint_plugin/octomap_planner_client.hpp
+++ include/octomap_waypoint_plugin/octomap_planner_client.hpp
@@ -1,14 +1,20 @@
 #ifndef OCTOMAP_PLANNER_HPP
 #define OCTOMAP_PLANNER_HPP
 
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "ros/forwards.h"
+#include "ros/service_client.h"
 #include <uav_ros_tracker/planner_interface.hpp>
+#include <deque>
+#include <mutex>
 
 namespace uav_ros_tracker {
 
 /**
  * @brief Environment aware planner plugin. Uses
- * https://github.com/larics/larics_motion_planning capabailities to plan a collision-free
- * path around an octomap.
+ * https://github.com/larics/larixcs_motion_planning capabailities to plan a
+ * collision-free path around an octomap.
  *
  */
 class OctomapPlannerClient : public planner_interface
@@ -21,11 +27,41 @@ public:
   geometry_msgs::PoseArray     getWaypointArray() override;
   uav_ros_msgs::WaypointStatus getWaypointStatus(const nav_msgs::Odometry& odom) override;
 
-  bool initialize(ros::NodeHandle& nh, ros::NodeHandle& nh_private) override;
+  bool initialize(
+    ros::NodeHandle&                                                 nh,
+    ros::NodeHandle&                                                 nh_private,
+    std::unordered_map<std::string, geometry_msgs::TransformStamped> transform_map,
+    std::string tracking_frame) override;
+
   std::tuple<bool, std::string, uav_ros_msgs::WaypointPtr> publishWaypoint(
     const nav_msgs::Odometry& current_odometry,
     bool                      tracking_enabled = true,
     bool                      control_enabled  = true) override;
+
+private:
+  static constexpr auto NAME         = "OctomapPlannerClient";
+  static constexpr auto DISTANCE_TOL = 0.3;
+
+  ros::Timer m_waiting_timer;
+  void       waiting_callback(const ros::TimerEvent& e);
+
+  ros::Timer m_planning_timer;
+  void       plannning_callback(const ros::TimerEvent& e);
+
+  geometry_msgs::PoseStamped m_carrot_pose;
+  std::mutex                 m_carrot_pose_mutex;
+  void                       carrot_pose_cb(const geometry_msgs::PoseStamped& pose);
+
+  bool                                                             m_is_flying  = false;
+  bool                                                             m_is_waiting = false;
+  std::unordered_map<std::string, geometry_msgs::TransformStamped> m_transform_map;
+  std::string                                                      m_tracking_frame;
+
+  std::mutex                            m_waypoint_buffer_mutex;
+  std::deque<uav_ros_msgs::WaypointPtr> m_waypoint_buffer;
+
+  ros::ServiceClient m_planner_client;
+  ros::Publisher     m_tracker_trajectory_pub;
 };
 }// namespace uav_ros_tracker
 
--- src/octomap_planner_client.cpp
+++ src/octomap_planner_client.cpp
@@ -1,18 +1,49 @@
+#include "geometry_msgs/PoseStamped.h"
+#include "ros/duration.h"
+#include "ros/forwards.h"
+#include <ios>
+#include <mutex>
 #include <octomap_waypoint_plugin/octomap_planner_client.hpp>
+#include <larics_motion_planning/MultiDofTrajectory.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
 
 void uav_ros_tracker::OctomapPlannerClient::addWaypoint(
   const uav_ros_msgs::Waypoint& waypoint)
-{}
+{
+  std::lock_guard<std::mutex> lock(m_waypoint_buffer_mutex);
+  m_waypoint_buffer.emplace_back(boost::make_shared<uav_ros_msgs::Waypoint>(waypoint));
+
+  ROS_INFO("[%s] Waypoint Added [%.2f, %.2f, %.2f]",
+           NAME,
+           waypoint.pose.pose.position.x,
+           waypoint.pose.pose.position.y,
+           waypoint.pose.pose.position.z);
+}
 
 void uav_ros_tracker::OctomapPlannerClient::addWaypoints(
   const uav_ros_msgs::Waypoints& waypoints)
-{}
+{
+  for (const auto& wp : waypoints.waypoints) { addWaypoint(wp); }
+}
 
-void uav_ros_tracker::OctomapPlannerClient::clearWaypoints() {}
+void uav_ros_tracker::OctomapPlannerClient::clearWaypoints()
+{
+  std::lock_guard<std::mutex> lock(m_waypoint_buffer_mutex);
+  m_waypoint_buffer.clear();
+}
 
 geometry_msgs::PoseArray uav_ros_tracker::OctomapPlannerClient::getWaypointArray()
 {
   geometry_msgs::PoseArray poseArray;
+
+  {
+    std::lock_guard<std::mutex> lock(m_waypoint_buffer_mutex);
+    for (const auto& waypoint : m_waypoint_buffer) {
+      auto transformed_wp =
+        transform_waypoint(*waypoint, m_transform_map, m_tracking_frame);
+      poseArray.poses.push_back(transformed_wp.pose.pose);
+    }
+  }
 
   return poseArray;
 }
@@ -21,14 +52,36 @@ uav_ros_msgs::WaypointStatus uav_ros_tracker::OctomapPlannerClient::getWaypointS
   const nav_msgs::Odometry& odom)
 {
   uav_ros_msgs::WaypointStatus waypointStatus;
-
+  waypointStatus.flying_to_wp  = m_is_flying;
+  waypointStatus.waiting_at_wp = m_is_waiting;
   return waypointStatus;
 }
 
-bool uav_ros_tracker::OctomapPlannerClient::initialize(ros::NodeHandle& nh,
-                                                       ros::NodeHandle& nh_private)
+bool uav_ros_tracker::OctomapPlannerClient::initialize(
+  ros::NodeHandle&                                                 nh,
+  ros::NodeHandle&                                                 nh_private,
+  std::unordered_map<std::string, geometry_msgs::TransformStamped> transform_map,
+  std::string                                                      tracking_frame)
 {
-  return false;
+  m_tracking_frame = std::move(tracking_frame);
+  m_transform_map  = std::move(transform_map);
+  m_planner_client =
+    nh.serviceClient<larics_motion_planning::MultiDofTrajectory>("octomap_planner");
+  m_tracker_trajectory_pub =
+    nh.advertise<trajectory_msgs::MultiDOFJointTrajectory>("tracker/input_trajectory", 1);
+
+  m_waiting_timer = nh.createTimer(ros::Duration(1),
+                                   &OctomapPlannerClient::waiting_callback,
+                                   this,
+                                   true /* oneshot */,
+                                   false /*autostart */);
+
+  m_planning_timer = nh.createTimer(ros::Duration(0.01),
+                                    &OctomapPlannerClient::plannning_callback,
+                                    this,
+                                    true /* oneshot */,
+                                    false /*autostart */);
+  return true;
 }
 
 std::tuple<bool, std::string, uav_ros_msgs::WaypointPtr>
@@ -37,7 +90,97 @@ std::tuple<bool, std::string, uav_ros_msgs::WaypointPtr>
     bool                      tracking_enabled,
     bool                      control_enabled)
 {
+
+  if (!control_enabled) {
+    // reset();
+    return std::make_tuple<bool, std::string, uav_ros_msgs::WaypointPtr>(
+      false, "UAV off-board control is disabled.", {});
+  }
+
+  // Get the latest waypoint
+  uav_ros_msgs::WaypointPtr current_waypoint_ptr;
+  {
+    std::lock_guard<std::mutex> lock(m_waypoint_buffer_mutex);
+
+    if (m_waypoint_buffer.empty()) {
+      // reset();
+      return std::make_tuple<bool, std::string, uav_ros_msgs::WaypointPtr>(
+        false, "No waypoints available.", {});
+    }
+
+    current_waypoint_ptr = m_waypoint_buffer.front();
+  }
+
+  if (current_waypoint_ptr == nullptr) {
+    return std::make_tuple<bool, std::string, uav_ros_msgs::WaypointPtr>(
+      false, "No waypoints available.", {});
+  }
+
+  auto distance_to_wp = this->calc_distance(current_odometry, *current_waypoint_ptr);
+  // If we are are flying and still haven't reached the distance
+  if (m_is_flying && distance_to_wp >= DISTANCE_TOL) {
+    return std::make_tuple(false, "Flying to current waypoint!", current_waypoint_ptr);
+  }
+
+  // We have reached the desired waypoint distance, start the waiting
+  if (m_is_flying && !m_is_waiting && distance_to_wp < DISTANCE_TOL) {
+    m_is_waiting = true;
+    m_is_flying  = false;
+    m_waiting_timer.setPeriod(ros::Duration(current_waypoint_ptr->waiting_time), true);
+    m_waiting_timer.start();
+    return std::make_tuple(
+      false, "Started waiting at the current waypoint!", current_waypoint_ptr);
+  }
+
+  // If we are not flying but rather waiting than
+  if (m_is_waiting) {
+    return std::make_tuple(
+      false, "Waiting at the current waypoint!", current_waypoint_ptr);
+  }
+
+  // Check if tracker is available
+  if (!tracking_enabled) {
+    // reset();
+    return std::make_tuple<bool, std::string, uav_ros_msgs::WaypointPtr>(
+      false, "Tracker is busy!", {});
+  }
+
+  if (!m_planning_timer.hasPending())
+  {
+    m_planning_timer.setPeriod(ros::Duration(0.01), true);
+  }
+
+  m_planning_timer.start();
   return { false, "Not implemented!", nullptr };
+}
+
+void uav_ros_tracker::OctomapPlannerClient::waiting_callback(const ros::TimerEvent& e)
+{
+  ROS_INFO("[%s] Waiting at waypoint finished!", NAME);
+  m_is_waiting = false;
+
+  {
+    std::lock_guard<std::mutex> lock(m_waypoint_buffer_mutex);
+    m_waypoint_buffer.pop_front();
+  }
+}
+
+void uav_ros_tracker::OctomapPlannerClient::plannning_callback(const ros::TimerEvent& e)
+{
+  ROS_INFO_THROTTLE(2.0, "[%s::planning_callback]", NAME);
+
+  if (!m_planner_client.exists()) {
+    ROS_WARN_THROTTLE(2.0, "[%s::planning_callback] Planning service is missing!", NAME);
+    return;
+  }
+
+  // TODO: do the planning here
+}
+
+void uav_ros_tracker::OctomapPlannerClient::carrot_pose_cb(const geometry_msgs::PoseStamped& msg)
+{
+  std::lock_guard<std::mutex> lock(m_carrot_pose_mutex);
+  m_carrot_pose = msg;
 }
 
 #include <pluginlib/class_list_macros.h>
